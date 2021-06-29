6ea58080a7a71e0c4ad0c27568dad73801f6add9
crystal993 <ggg7152@gmail.com>
2021-06-29 17:30:59 +0900

    Title : javascript util 현재시간, 날짜 출력 코드 최적화
    slice() 이용

--- 05.js_basic/src/main/webapp/js/common.js
+++ 05.js_basic/src/main/webapp/js/common.js
@@ -26,31 +26,31 @@ getDate()
 */
 function getCurrentDate() {
 	var currentDate = new Date();
-	var year = currentDate.getFullYear();
+	var year = (currentDate.getFullYear());
 
-	var month = currentDate.getMonth() + 1;
-	if (month <= 9) {
-		month = "0" + month;		
-	}
+	var month = ("0" + (currentDate.getMonth()+1)).slice(-2);
+
+	var date = ("0" + currentDate.getDate()).slice(-2);
 	
-	var date = currentDate.getDate();
-	
-	var date = currentDate.getDate() + 1;
-	if (date <= 9) {
-		date = "0" + date;		
-	}
-	
-	currentDate = year + "-" + month + "-" + date;
-	console.log("currentDate: "+currentDate);
-	
-	return currentDate;
+	return year + "-" + month + "-" + date;
 }
 
 
 
-/* 현재 시간 시간2자리 : 분 2자리 : 초2자리 기본형식의 날짜 반환 함수 */
+/* 현재 시간 시간2자리 : 분 2자리 : 초2자리 기본형식의 날짜 반환 함수 
+	-- 01, 02, .. 09, 010, 011, 012
+	-- ('0' + 시간) => 뒤에서 2자리 가져오기
+*/
+
 function getCurrentTime() {
-	document.write(d.getHours()+" : "+d.getMinutes()+" : "+d.getSeconds() )
+	var currentTime = new Date();
+	var hours = ("0" + currentTime.getHours()).slice(-2);
+
+	var minutes = ("0" + currentTime.getMinutes()).slice(-2);
+	
+	var seconds = ("0" + currentTime.getSeconds()).slice(-2);
+	
+	return hours + ":" + minutes + ":" + seconds;
 }
 
 
